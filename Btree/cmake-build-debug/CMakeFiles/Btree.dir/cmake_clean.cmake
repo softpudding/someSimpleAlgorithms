7bd1bde34f3af923d1477963726e88f8193321a0
yy <37563992+llIllIllIlllIll@users.noreply.github.com>
2018-05-29 19:06:59 +0800

    btree

---
+++ Btree/cmake-build-debug/CMakeFiles/Btree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Btree.dir/main.cpp.obj"
+  "Btree.pdb"
+  "Btree.exe"
+  "Btree.exe.manifest"
+  "libBtree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Btree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- redBlackTree/cmake-build-debug/cmake_install.cmake
+++ redBlackTree/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/11570/CLionProjects/algorithm/redBlackTree
+# Install script for directory: C:/Users/11570/Documents/GitHub/someSimpleAlgorithms/redBlackTree
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/11570/CLionProjects/algorithm/redBlackTree/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/11570/Documents/GitHub/someSimpleAlgorithms/redBlackTree/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
